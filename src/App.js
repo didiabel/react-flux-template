ab20be1e6f33d9ed04b7d3c80adf576ec8d448c6
rosinni <ros.rm169@gmail.com>
2021-08-11 17:55:32 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import logo from '../src/img/logo-circular-texto.png';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from "./views/Home"
+import Demo from "./views/Demo"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
+  
   return (
-    <div className="container-fluid">
-      <header className="container-fluid d-flex flex-column align-items-center mt-5">
-      <h1 className="text-center">Bienvenido al template simple de React</h1>
-         <div className="w-25">
-        <img src={logo} className="w-100" alt="logo" />
-         </div>
-        <p>
-          Edita <code>src/App.js</code> y salva el cambio para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendé React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Navbar/>
+      <Switch>
+        <Route exact path="/">
+        <Home/>
+        </Route>
+        <Route exact path="/demo">
+          <Demo/>
+        </Route>
+      </Switch>
+      <Footer/>
+    </Router>
   );
 }
 
